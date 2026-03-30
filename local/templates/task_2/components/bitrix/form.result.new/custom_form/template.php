e394e29787caa6c3379a9ac131a98e059ae8e558
anton-tt <carduelis@yandex.ru>
2026-03-30 12:12:23 +0700

    refactor: В шаблон перенесена и адаптрироана разметка для формы

--- local/templates/task_2/components/bitrix/form.result.new/custom_form/template.php
+++ local/templates/task_2/components/bitrix/form.result.new/custom_form/template.php
@@ -1,3 +1,37 @@
 <?php
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 ?>
+
+<?=$arResult["FORM_HEADER"]?>
+
+<div class="contact-form">
+    <div class="contact-form__head">
+        <div class="contact-form__head-title">Связаться</div>
+        <div class="contact-form__head-text">Наши сотрудники помогут выполнить подбор услуги и&nbsp;расчет цены с&nbsp;учетом
+            ваших требований
+        </div>
+    </div>
+
+    <div class="contact-form__form">
+        <div class="contact-form__form-inputs">
+
+        </div>
+
+        <div class="contact-form__form-message">
+
+        </div>
+
+        <div class="contact-form__bottom">
+            <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что
+                ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных
+                данных&raquo;.
+            </div>
+            <button class="form-button contact-form__bottom-button" data-success="Отправлено" data-error="Ошибка отправки">
+                <div class="form-button__title">Оставить заявку</div>
+            </button>
+        </div>
+    </div>
+    
+</div>
+
+<?=$arResult["FORM_FOOTER"]?>
