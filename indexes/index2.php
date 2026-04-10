6732a5dbfdbe31e337c37261c5b0f723634a8163
anton-tt <carduelis@yandex.ru>
2026-04-11 01:12:13 +0700

    refactor: файлы index.php от первого и второго заданий вынесены в отдельную директорию

---
+++ indexes/index2.php
@@ -0,0 +1,23 @@
+<?
+define("SITE_TEMPLATE_ID", "task_2");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Новости компании"); ?>
+
+<? $APPLICATION->IncludeComponent(
+	"bitrix:form.result.new",
+	"custom_form",
+	array(
+		"WEB_FORM_ID" => "1",
+		"IGNORE_CUSTOM_TEMPLATE" => "N",
+		"USE_EXTENDED_ERRORS" => "Y",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "3600",
+		"LIST_URL" => "",
+		"EDIT_URL" => "",
+		"SUCCESS_URL" => "",
+		"CHAIN_ITEM_TEXT" => "",
+		"CHAIN_ITEM_LINK" => "",
+		"VARIABLE_ALIASES" => array()
+	)
+); ?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
