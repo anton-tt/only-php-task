6364deeb649c9be8a1a780598083cd915ab98591
anton-tt <carduelis@yandex.ru>
2026-04-12 20:41:59 +0700

    feat: Подготовлена подробная страница новости на detail.php

--- local/templates/task_3/components/bitrix/news/custom_news/detail.php
+++ local/templates/task_3/components/bitrix/news/custom_news/detail.php
@@ -1,3 +1,20 @@
 <?php
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 ?>
+
+<div class="article-card">
+    <div class="article-card__title"><?= htmlspecialchars($arResult["NAME"]) ?></div>
+    <div class="article-card__date"><?= htmlspecialchars($arResult["DISPLAY_ACTIVE_FROM"]) ?></div>
+    <div class="article-card__content">
+        <?php if ($arResult["DETAIL_PICTURE"]): ?>
+            <div class="article-card__image sticky">
+                <img src="<?= $arResult["DETAIL_PICTURE"]["SRC"] ?>" alt="" data-object-fit="cover" />
+            </div>
+        <?php endif; ?>
+        
+        <div class="article-card__text">
+            <div class="block-content" data-anim="anim-3"><?= $arResult["DETAIL_TEXT"] ?></div>
+            <a class="article-card__button" href="<?= $arResult["LIST_PAGE_URL"] ?>">Назад к новостям</a>
+        </div>
+    </div>
+</div>
